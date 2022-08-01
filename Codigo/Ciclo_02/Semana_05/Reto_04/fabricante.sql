70da257380679c4a927de8abc723c04710b6de82
Christian Bermúdez-Rivas <41581329+ChrisBermudezR@users.noreply.github.com>
2022-08-01 12:01:02 -0500

    Adición de reto 04

---
+++ Codigo/Ciclo_02/Semana_05/Reto_04/fabricante.sql
@@ -0,0 +1,21 @@
+create table fabricante (
+
+    fab_id  int primary key,
+
+    fab_nombre  char(20),
+
+    fab_direccion char(45),
+
+    fab_telefono  char(20)
+
+);
+
+insert into fabricante values (101, "Auteco","calle 7 No. 45-17", "05713224459");
+
+insert into fabricante values (102, "Hitachi", "calle 19 No. 108-26", "05714223344");
+
+insert into fabricante values (103, "Bosch", "carrera 68 No. 26-45", "05715678798");
+
+insert into fabricante values (104, "Teco", "calle 77 No. 68-33", "05712213243");
+
+insert into fabricante values (105, "General Electric", "calle 29 No. 26-12", "05717239919");
