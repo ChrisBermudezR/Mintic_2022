b17c3f29a2cf88c9b795d901e31b7efa66540b81
Christian Bermúdez-Rivas <chrisbermudezr@gmail.com>
2022-08-02 22:33:37 -0500

    Adición de reto 05

---
+++ Codigo/Ciclo_02/Semana_06/Reto_05/Reto_5/src/reto_5/Principal.java
@@ -0,0 +1,55 @@
+package reto_5;
+import java.util.Scanner;
+import java.sql.Connection;
+public class Principal {
+     public static Scanner sc = new Scanner(System.in);
+     
+     public static void main(String[] args){
+     
+     menu();
+     }
+     
+     
+     public static void menu(){
+     try{
+         System.out.println("******\n"+
+                 "1. Crear tablas. \n"+
+                 "2. Ingresar tabla de datos  \n"+
+                 "3. Ingresar tabla de datos   \n"+
+                 "4. Ingresar tabla de datos   \n"+
+                 "5. Ingresar tabla de datos   \n"+
+                 "6. Ingresar tabla de datos   \n"+
+                 "7. Ingresar tabla de datos   \n"+
+                 "8. Ingresar tabla de datos   \n"+
+                 "9. Ingresar tabla de datos   \n"+
+                 "10. Ingresar tabla de datos   \n"+
+                 "11. Ingresar tabla de datos   \n"+
+                 "12. Ingresar tabla de datos   \n"+
+                 "13. Ingresar tabla de datos   \n"+
+                 "14. Ingresar tabla de datos   \n"+
+                 "15. Ingresar tabla de datos   \n"+
+                 "16. Ingresar tabla de datos   \n"+
+                 
+                 "Otro número para salir"
+         
+         
+         ); 
+             
+         
+                                 
+         
+     }
+     
+     catch(Exception e){
+     
+     }
+     
+     
+     }
+     
+     public static Connection conectar(){
+     
+         return null;
+     
+     }
+}
